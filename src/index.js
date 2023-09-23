6bb9d642d3625d1bc119ebbd2c5d37040487b8e9
Jasurbek <jasurdev1604@gmail.com>
2023-09-24 01:49:15 +0500

    Houzing

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <>
-      <h1>Jasurbek </h1>
+      <h1>Jasurbek O'telbayev</h1>
     </>
   </React.StrictMode>
 );
