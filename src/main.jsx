63a6f0608637a365698909a3b351aeb100b5b1ec
jasurbek <jasurdev1604@gmail.com>
2023-11-07 17:08:50 +0500

    project configure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./root/index.jsx";
+import "./index.css";
+import RootContext from "./context/index.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RootContext>
+      <App />
+    </RootContext>
+  </React.StrictMode>
+);
